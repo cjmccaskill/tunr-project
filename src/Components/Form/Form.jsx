e4bef434310fd3b56e22f4b00c4ba5ad711fd1c5
traceannwhite <traceannwhite@gmail.com>
2021-08-19 15:26:08 -0500

    passed song props to form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Form.css";
 
 const Form = (props) => {
-  const [formData, setFormData] = useState([]);
+  const [formData, setFormData] = useState(props.song);
 
   const handleSubmit = (event) => {
     event.preventDefault();
