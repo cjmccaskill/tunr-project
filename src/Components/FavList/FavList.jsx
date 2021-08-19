64041dcc01d2658b9468d5518e2fb8d3123aaec0
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-19 14:00:47 -0500

    features-fav - add file structure for favlist

--- src/Components/FavList/FavList.jsx
+++ src/Components/FavList/FavList.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./FavList.css";
 
-const FavList = () => {
+const FavList = (props) => {
+  const loaded = props.favs.map((fav) => {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <div className="fav" key={fav._id}>
+        <section>
+          <span className="fav-title">
+            <strong>Title:</strong>
+            {fav.title}
+          </span>
+          <span className="fav-artist">
+            <strong>Artist:</strong>
+            {fav.artist}
+          </span>
+          <span className="fav-time">
+            <strong>Time:</strong>
+            {fav.time}
+          </span>
+        </section>
+      </div>
+    );
+  });
 
-export default FavList
+  const loading = "Loading...";
+
+  return (
+    <div>
+      <h3>My Fav List</h3>
+      {props.favs.length > 0 ? loaded : loading}
+    </div>
+  );
+};
+
+export default FavList;
