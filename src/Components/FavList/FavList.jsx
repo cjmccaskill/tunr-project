3e9892194bb55cceec3a2b0221597f8fe55177b2
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-19 14:53:06 -0500

    features-favlist - add file structure for favs list

--- src/Components/FavList/FavList.jsx
+++ src/Components/FavList/FavList.jsx
@@ -2,35 +2,35 @@ import React from "react";
 import "./FavList.css";
 
 const FavList = (props) => {
-  const loaded = props.favs.map((fav) => {
-    return (
-      <div className="fav" key={fav._id}>
-        <section>
-          <span className="fav-title">
-            <strong>Title:</strong>
-            {fav.title}
-          </span>
-          <span className="fav-artist">
-            <strong>Artist:</strong>
-            {fav.artist}
-          </span>
-          <span className="fav-time">
-            <strong>Time:</strong>
-            {fav.time}
-          </span>
-        </section>
-      </div>
-    );
-  });
+  const { favs } = props;
+
+  const loaded = () => (
+    <div>
+      <h3>My Fav List</h3>
+      {favs.map((fav) => (
+        <div className="fav" key={fav._id}>
+          <section>
+            <span className="fav-title">
+              <strong>Title:</strong>
+              {fav.title}
+            </span>
+            <span className="fav-artist">
+              <strong>Artist:</strong>
+              {fav.artist}
+            </span>
+            <span className="fav-time">
+              <strong>Time:</strong>
+              {fav.time}
+            </span>
+          </section>
+        </div>
+      ))}
+    </div>
+  );
 
   const loading = "Loading...";
 
-  return (
-    <div>
-      <h3>My Fav List</h3>
-      {props.favs.length > 0 ? loaded : loading}
-    </div>
-  );
+  return FavList.length > 0 ? loaded : loading;
 };
 
 export default FavList;
